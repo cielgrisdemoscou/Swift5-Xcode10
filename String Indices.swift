18b6062a52a1f04d6732900a5dfebfc53a977794
Ted Hyeong <50784573+cielgrisdemoscou@users.noreply.github.com>
2019-07-23 12:07:41 +0900

    Add files via upload

---
+++ String Indices.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+// String Indices 
+
+let str = "Swift"
+
+let firstCh = str[str.startIndex]
+print(firstCh)
+
+// let lastCh = str[str.endIndex] X
+let lastCharIndex = str.index(before: str.endIndex)
+let lastCh = str[lastCharIndex]
+print(lastCh)
+
+let secondCharIndex = str.index[after: str.startIndex]
+let secondCh = str[secondCharIndex]
+print(secondCh)
+
+var thirdCharIndex = str.index(str.startIndex, offsetBy: 2)
+var thirdCh = str[thirdCharIndex]
+print(thirdCh)
+
+thirdCharIndex = str.index(str.endIndex, offsetBy: -3)
+thirdCh = str[thirdCharIndex]
+print(thirdCh)
+
