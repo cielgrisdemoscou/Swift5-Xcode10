ee46b6b001f03eb6dfb271934da7b29c8db0901e
Ted Hyeong <50784573+cielgrisdemoscou@users.noreply.github.com>
2019-07-27 23:10:30 +0900

    Add files via upload

---
+++ Control Flow - Control Transfer Statements.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+//Control Flow 
+//Control Transfer Statements
+//fallthrough
+
+let integerToDescribe = 5
+var description = "The number \(integerToDescribe) is"
+switch integerToDescribe {
+case 2, 3, 5, 7, 11, 13, 17, 19:
+    description += " a prime number, and also"
+    fallthrough
+default:
+    description += " an integer."
+}
+print(description)
+// Prints "The number 5 is a prime number, and also an integer."
+
+//Early Exit
+
+func greet(person: [String: String]) {
+    guard let name = person["name"] else {
+        return
+    }
+
+    print("Hello \(name)!")
+
+    guard let location = person["location"] else {
+        print("I hope the weather is nice near you.")
+        return
+    }
+
+    print("I hope the weather is nice in \(location).")
+}
+
+greet(person: ["name": "John"])
+// Prints "Hello John!"
+// Prints "I hope the weather is nice near you."
+greet(person: ["name": "Jane", "location": "Cupertino"])
+// Prints "Hello Jane!"
+// Prints "I hope the weather is nice in Cupertino."
+
+
