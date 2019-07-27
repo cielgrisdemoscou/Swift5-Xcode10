081bf885022db48d52c29e5d0f37cd520890a540
Ted Hyeong <50784573+cielgrisdemoscou@users.noreply.github.com>
2019-07-27 22:59:35 +0900

    Add files via upload

---
+++ Control Flow - Compound Cases.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+//Control Flow 
+//where
+
+let yetAnotherPoint = (1, -1)
+switch yetAnotherPoint {
+case let (x, y) where x == y:
+    print("(\(x), \(y)) is on the line x == y")
+case let (x, y) where x == -y:
+    print("(\(x), \(y)) is on the line x == -y")
+case let (x, y):
+    print("(\(x), \(y)) is just some arbitrary point")
+}
+// Prints "(1, -1) is on the line x == -y"
+
+//Compound Cases
+
+let stillAnotherPoint = (9, 0)
+switch stillAnotherPoint {
+case (let distance, 0), (0, let distance):
+    print("On an axis, \(distance) from the origin")
+default:
+    print("Not on an axis")
+}
+// Prints "On an axis, 9 from the origin"
+
+
