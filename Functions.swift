9bc509fd6cd8773fedd2f3c9b731f12db3490296
Ted Hyeong <50784573+cielgrisdemoscou@users.noreply.github.com>
2019-07-28 09:54:05 +0900

    Add files via upload

---
+++ Functions.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+// Functions 
+// Using Function Types 
+
+
+func addTwoInts(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+
+var mathFunction: (Int, Int) -> Int = addTwoInts
+
+print("Result: \(mathFunction(2, 3))")
+// Prints "Result: 5"
+
+// Function Types as Parameter Types
+
+
+func printMathResult(_ mathFunction: (Int, Int) -> Int, _ a: Int, _ b: Int) {
+    print("Result: \(mathFunction(a, b))")
+}
+printMathResult(addTwoInts, 3, 5)
+// Prints "Result: 8"
