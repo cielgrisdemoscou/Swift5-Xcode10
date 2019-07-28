80d504b900518da73fdb615d8bb4cfb8ef1d1c93
Ted Hyeong <50784573+cielgrisdemoscou@users.noreply.github.com>
2019-07-28 09:39:23 +0900

    Add files via upload

---
+++ Labeled Statements.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+// Labeled Statements
+
+outer: for i in 1...3 {
+    print("OUTER LOOP", i)
+
+    for j in 1...3 {
+        print("     inner loop", j)
+        break outer
+    }
+}
+
+//label name: while condition {
+   //statements
+//}
