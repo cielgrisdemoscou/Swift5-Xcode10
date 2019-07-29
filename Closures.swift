ea0ad918de755a421af146bcdab44461c2c41820
Ted Hyeong <50784573+cielgrisdemoscou@users.noreply.github.com>
2019-07-29 23:20:23 +0900

    Update Closures.swift

--- Closures.swift
+++ Closures.swift
@@ -23,3 +23,27 @@ perform(closure: { (str: String) -> String in
     return "Hi, \(str)"    
 })
 
+let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+
+func backward(_ s1: String, _ s2: String) -> Bool {
+    return s1 > s2
+}
+var reversedNames = names.sorted(by: backward)
+// reversedNames is equal to ["Ewa", "Daniella", "Chris", "Barry", "Alex"]
+
+reversedNames = names.sorted(by: { (s1: String, s2: String) -> Bool in
+    return s1 > s2
+})
+
+reversedNames = names.sorted(by: { s1, s2 in return s1 > s2 } )
+
+reversedNames = names.sorted(by: { s1, s2 in s1 > s2 } )
+
+reversedNames = names.sorted(by: { $0 > $1 } )
+
+reversedNames = names.sorted(by: >)
+
+// Trailling Closures 
+reversedNames = names.sorted() { $0 > $1 }
+
+reversedNames = names.sorted { $0 > $1 }
