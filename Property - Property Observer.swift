6d9190734a0bf75c469dd37a21609de6c5ee297d
Ted Hyeong <50784573+cielgrisdemoscou@users.noreply.github.com>
2019-07-30 15:24:03 +0900

    Update Property - Property Observer.swift

--- Property - Property Observer.swift
+++ Property - Property Observer.swift
@@ -27,3 +27,33 @@ s.width = 123
 //0.0 => 123.0
 //0.0 => 123.0
 
+/*
+willSet : 값이 저장되기 바로 직전에 호출 됨
+didSet : 새 값이 저장되고 난 직후에 호출 됨
+willSet에서는 새 값의 파라미터명을 지정할 수 있는데, 지정하지 않으면 기본 값으로 newValue를 사용합니다.
+didSet에서는 바뀌기 전의 값의 파라미터명을 지정할 수 있는데, 지정하지 않으면 기본 값으로 oldValue를 사용합니다.
+
+*/
+
+class StepCounter {
+    var totalSteps: Int = 0 {
+        willSet(newTotalSteps) {
+            print("About to set totalSteps to \(newTotalSteps)")
+        }
+        didSet {
+            if totalSteps > oldValue  {
+                print("Added \(totalSteps - oldValue) steps")
+            }
+        }
+    }
+}
+let stepCounter = StepCounter()
+stepCounter.totalSteps = 200
+// About to set totalSteps to 200
+// Added 200 steps
+stepCounter.totalSteps = 360
+// About to set totalSteps to 360
+// Added 160 steps
+stepCounter.totalSteps = 896
+// About to set totalSteps to 896
+// Added 536 steps
