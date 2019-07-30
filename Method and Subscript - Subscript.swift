322b1e418c0dc95e33bf12b06fe12f7a1fa4f15b
Ted Hyeong <50784573+cielgrisdemoscou@users.noreply.github.com>
2019-07-30 19:12:27 +0900

    Update Method and Subscript - Subscript.swift

--- Method and Subscript - Subscript.swift
+++ Method and Subscript - Subscript.swift
@@ -48,3 +48,13 @@ struct Matrix {
 
 let m = Matrix()
 print(m[0, 0])
+
+struct TimesTable {
+    let multiplier: Int
+    subscript(index: Int) -> Int {
+        return multiplier * index
+    }
+}
+
+let threeTimesTable = TimesTable(multiplier: 3)
+print(threeTimesTable[7])
