1ec8f5905d0f54729ef0f4d18887f740827e38c0
Ted Hyeong <50784573+cielgrisdemoscou@users.noreply.github.com>
2019-07-31 14:14:11 +0900

    Update Inheritance and Polymorphism - Inheritance.swift

--- Inheritance and Polymorphism - Inheritance.swift
+++ Inheritance and Polymorphism - Inheritance.swift
@@ -43,4 +43,15 @@ final class Rectangle: Figure {
 //class Square: Rectangle {
 //} if we use final class, we can't use Inheritance.    
 
+class Vehicle {
+    var currentSpeed = 0.0
+    var description: String {
+        return "traveling at \(currentSpeed) miles per hour"
+    }
+    func makeNoise() {
+        // do nothing - an arbitrary vehicle doesn't necessarily make a noise
+    }
+}
 
+print("Vehicle: \(someVehicle.description)")
+// Vehicle: traveling at 0.0 miles per hour
