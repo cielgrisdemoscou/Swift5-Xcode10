e1f9c13f004ccfef4feb98d7bf6a9f5f4f2f6565
Ted Hyeong <50784573+cielgrisdemoscou@users.noreply.github.com>
2019-07-31 22:45:47 +0900

    Add files via upload

---
+++ Initializer - Optional Property Types.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+// Initializer
+// Optional Property Types
+
+class SurveyQuestion {
+    var text: String
+    var response: String?
+    init(text: String) {
+        self.text = text
+    }
+    func ask() {
+        print(text)
+    }
+}
+let cheeseQuestion = SurveyQuestion(text: "Do you like cheese?")
+cheeseQuestion.ask()
+// Prints "Do you like cheese?"
+cheeseQuestion.response = "Yes, I do like cheese."
+
+let beetsQuestion = SurveyQuestion(text: "How about beets?")
+beetsQuestion.ask()
+// Prints "How about beets?"
+beetsQuestion.response = "I also like beets. (But not with cheese.)"
+
+
+
